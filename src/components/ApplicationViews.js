fba201c157bf2f0ed38000419d26278f922b74ae
yasmeencole <yasmeencole1@mail.com>
2021-05-04 15:17:58 -0500

    modified appviews

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,12 +2,18 @@ import React from "react"
 import { Route } from "react-router-dom"
 
 export const ApplicationViews = () => {
-    return <>
+    return (
+    <>
         <main style={{
             margin: "5rem 2rem",
             backgroundColor: "lightgoldenrodyellow"
         }}>
             Application views
         </main>
+
+        <Route exact path="/">
+            
+        </Route>
     </>
+    )
 }
